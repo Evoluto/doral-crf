b25fd1081f7ac600fa8c93ab8663221ab724f42e
Prabhjeet Singh <prabhjeet.singh@daffodilsw.com>
2020-09-30 19:44:49 +0530

    clear storage on init

--- src/app/modules/authentication/components/registration/registration.component.ts
+++ src/app/modules/authentication/components/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -7,7 +7,7 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css'],
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
   user = { email: '', password: '', cpassword: '' };
   errMsg = '';
   termsChecked: boolean = true;
@@ -16,6 +16,10 @@ export class RegistrationComponent {
     private authService: AuthService,
     private routes: Router
   ) {}
+
+  ngOnInit() {
+    localStorage.clear();
+  }
 
   onSubmit() {
     this.errMsg = '';
