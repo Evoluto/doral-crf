961beff0b03715245e9c8fec8b75a02fe3961eab
IAF Consulting, LLC <mark@iafconsult.com>
2020-09-30 23:43:38 -0500

    user register implementation with custom api

--- src/app/modules/authentication/components/registration/registration.component.ts
+++ src/app/modules/authentication/components/registration/registration.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
-  user = { email: '', password: '', cpassword: '' };
+  user = { email: '', password: '', cpassword: '', roleId: '' };
   errMsg = '';
   termsChecked: boolean = true;
 
@@ -22,47 +22,26 @@ export class RegistrationComponent implements OnInit {
   }
 
   onSubmit() {
+    console.log(this.user);
     this.errMsg = '';
     if(! this.termsChecked) {
       this.errMsg = 'Please agree to all Tearms';
       return;
     }
-    this.login();
+    this.register();
   }
 
   register() {
     this.authService
       .register(this.user)
       .subscribe((res) => {
-        if (res) {
+        
           this.authService.user.next(null);
           localStorage.clear();
           this.routes.navigate(['/login']);
-        } else {
-          this.errMsg = 'Error while Registration';
-        }
+        
       }, error => {
         this.errMsg = 'Error while Registration';
       });
   }
-
-  login() {
-    // const username = 'register_user@mail.com';
-    // const password = 'rU$loraD1001!';
-    const username = 'super_gaurav@mail.com';
-    const password = 'Summer@1234';
-    this.authService
-      .signIn(username, password)
-      .subscribe((user) => {
-        if (user) {
-          this.register();
-        } else {
-          this.errMsg = 'Internal Server Error';
-        }
-      }, error => {
-        this.errMsg = 'Internal Server Error';
-      });
-  }
-
-
 }
