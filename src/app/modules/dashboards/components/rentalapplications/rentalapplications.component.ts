d1eaf7b994aa84dee0575ec216398d6591b58ef0
Gaurav Srivastava <gaurav.srivastava@daffodilsw.com>
2020-11-25 19:45:02 +0530

    Back button and filter changes

--- src/app/modules/dashboards/components/rentalapplications/rentalapplications.component.ts
+++ src/app/modules/dashboards/components/rentalapplications/rentalapplications.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
@@ -12,6 +12,7 @@ import { PopupModel } from 'src/app/modules/dashboards/models/popup';
 import { FormActionData, FieldListItem, Where } from 'src/app/models/form-action-data';
 import { StorageService } from 'src/app/services/storage.service';
 import { Subject } from 'rxjs';
+import { DataTableDirective } from 'angular-datatables';
 
 @Component({
   selector: 'app-rentalapplications',
@@ -31,9 +32,17 @@ export class RentalapplicationsComponent implements OnInit {
     private storageService: StorageService
   ) { }
 
-  modelConfig:PopupModel;
+  modelConfig: PopupModel;
   rentalApplicationList: Array<any> = [];
   rows: Array<any> = [];
+
+  statusList: Array<any> = [];
+  reviewStatusList: Array<any> = [];
+  reviewIssuesList: Array<any> = [];
+
+  selectedStatusList: Array<any> = [];
+  selectedReviewStatusList: Array<any> = [];
+  selectedReviewIssuesList: Array<any> = [];
 
   dtOptions: DataTables.Settings = {
     pagingType: 'full_numbers',
@@ -44,12 +53,20 @@ export class RentalapplicationsComponent implements OnInit {
       orderable: false
     }]
   };
+  @ViewChild(DataTableDirective, { static: false }) dtElement: DataTableDirective;
+  dtTrigger = new Subject();
+
 
   ngOnInit() {
     const componentData = this.route.snapshot.data['componentData'];
     this.rentalApplicationList = componentData[0];
     this.rows = this.rentalApplicationList;
-    this.spinner.hide()
+    this.setupFilters();
+  }
+
+  ngAfterViewInit(){
+    this.dtTrigger.next();
+    this.spinner.hide();
   }
 
   viewRentalRecord(id) {
@@ -59,6 +76,57 @@ export class RentalapplicationsComponent implements OnInit {
   openFilter(content) {
     this.modelConfig = new PopupModel();
     this.ngbModal.open(content, this.modelConfig.settings)
+  }
+
+  setupFilters() {
+    for (const iterator of this.rentalApplicationList) {
+
+      if (iterator.status && !this.statusList.includes(iterator.status)) {
+        this.statusList.push(iterator.status)
+      }
+
+      if (iterator.review_status && !this.reviewStatusList.includes(iterator.review_status)) {
+        this.reviewStatusList.push(iterator.review_status)
+      }
+
+      if (iterator.review_issue_reason && !this.reviewIssuesList.includes(iterator.review_issue_reason)) {
+        this.reviewIssuesList.push(iterator.review_issue_reason)
+      }
+
+    }
+  }
+
+  applyFilter() {
+
+    this.spinner.show()
+
+    const temp = this.rentalApplicationList.filter(iterator => {
+      const condition = (
+        (
+          this.selectedStatusList.length === 0 ||
+          this.selectedStatusList.includes(iterator.status)
+        ) &&
+        (
+          this.selectedReviewStatusList.length === 0 ||
+          this.selectedReviewStatusList.includes(iterator.review_status)
+        ) &&
+        (
+          this.selectedReviewIssuesList.length === 0 ||
+          this.selectedReviewIssuesList.includes(iterator.review_issue_reason)
+        )
+      )
+      return condition;
+    });
+
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      dtInstance.destroy();
+      setTimeout(() => {
+        this.dtTrigger.next();
+        this.ngbModal.dismissAll()
+        this.spinner.hide();
+      }, 500);
+    });
+    this.rows = [...temp];
   }
 
 }
